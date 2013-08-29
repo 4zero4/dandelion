c5d17b4d29bfa504701aaaacdb7d05ce8b34519b
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2013-08-29 19:09:15 -0300

    Consertado

--- src/dandObjects/SolarSys.java
+++ src/dandObjects/SolarSys.java
@@ -1,9 +1,11 @@
+/**
+ * Classe responsÃ¡vel pelos sistemas solares.
+ */
 package dandObjects;
 
 import java.util.*;
 
 /**
- * Classe responsável pelos sistemas solares.
  * @author Gabriel
  *
  */
@@ -26,83 +28,46 @@ public class SolarSys {
 	 */
 	public SolarSys(int i,double sysX, double sysY, double sysZ, double quadrantSeed,
 			String quadrantName) {
+		
 		//=====
 		//Seta as coordenadas do sistema.
 		//=====
 		this.solarSysCoord[0]=sysX;
 		this.solarSysCoord[1]=sysY;
 		this.solarSysCoord[2]=sysZ;
+		
 		//=====
-		//Seta a seed única para o sistema
+		//Seta a seed Ãºnica para o sistema
 		//=====
-		this.solarSysSeed=((sysX+sysY*2+sysZ*3*quadrantSeed)/6);
+		this.solarSysSeed=((sysX+sysY*2+sysZ*3+quadrantSeed)/6);
+		
 		//=====
 		//Seta o nome do sistema.
 		//=====
 		i+=1;
 		this.solarSysName=(quadrantName+"-"+i);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	}
-	
-	public void generate() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
-	/**
-	 * Retorna o nome do sistema solar
-	 * @return solarSysName
-	 */
 	public String getSolarSysName(){
 		return this.solarSysName;
 	}
 	
-	/**
-	 * Retorna um array com as coordenadas do sistema solar
-	 * @return solarSysCoord[]
-	 */
-	public double[] getSolarSysXCoord(){
-		return this.solarSysCoord;
+	public double getSolarSysXCoord(){
+		return this.solarSysCoord[0];
 	}
 	
-	/**
-	 * Retorna a seed do sistema solar
-	 * @return
-	 */
+	public double getSolarSysYCoord(){
+		return this.solarSysCoord[1];
+	}
+	
+	public double getSolarSysZCoord(){
+		return this.solarSysCoord[2];
+	}
+	
 	public double getSolarSysSeed(){
 		return this.solarSysSeed;
 	}
-=======
-		
-		System.out.println("Estrela:"+this.solarSysName+" criada!\nSeed:"+this.solarSysSeed+"\nX:"+this.solarSysCoord[0]+"\nY:"+this.solarSysCoord[1]+"\nZ:"+this.solarSysCoord[2]+"");
-		System.out.println("==================================================");
-		
-	}
->>>>>>> parent of 1a6b1e3... Mais coisas
-
-=======
-		
-		System.out.println("Estrela:"+this.solarSysName+" criada!\nSeed:"+this.solarSysSeed+"\nX:"+this.solarSysCoord[0]+"\nY:"+this.solarSysCoord[1]+"\nZ:"+this.solarSysCoord[2]+"");
-		System.out.println("==================================================");
-		
-	}
->>>>>>> parent of 1a6b1e3... Mais coisas
-=======
-		
-		System.out.println("Estrela:"+this.solarSysName+" criada!\nSeed:"+this.solarSysSeed+"\nX:"+this.solarSysCoord[0]+"\nY:"+this.solarSysCoord[1]+"\nZ:"+this.solarSysCoord[2]+"");
-		System.out.println("==================================================");
-		
-	}
->>>>>>> parent of 1a6b1e3... Mais coisas
-=======
-		
-		System.out.println("Estrela:"+this.solarSysName+" criada!\nSeed:"+this.solarSysSeed+"\nX:"+this.solarSysCoord[0]+"\nY:"+this.solarSysCoord[1]+"\nZ:"+this.solarSysCoord[2]+"");
-		System.out.println("==================================================");
-		
-	}
->>>>>>> parent of 1a6b1e3... Mais coisas
 
 }
